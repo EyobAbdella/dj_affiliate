3d54d24652a29ba46c563da0ae65a6d9effb092c
Eyob Abdella <eyobabdellasharo@gmail.com>
2025-01-16 12:32:53 +0300

    enhance admin fields

--- dj_affiliate/admin.py
+++ dj_affiliate/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 from dj_affiliate.models import Affiliate
+from django import forms
 
+class AffiliateForm(forms.ModelForm):
+    class Meta:
+        model = Affiliate
+        exclude = ['reward_amount']
 
 class AffiliateAdmin(admin.ModelAdmin):
-    def get_exclude(self, request, obj=None):
-        if request.method == 'POST':
-            return ("reward_amount",)
-        return None
+    form = AffiliateForm 
+    
+    def get_readonly_fields(self, request, obj=None):
+        if obj: 
+            return ['reward_amount']
+        return [] 
+
+    list_display = ['affiliate_id', 'user', 'reward_amount', 'created_at']
+    search_fields = ['affiliate_id', 'user__username']
 
 admin.site.register(Affiliate, AffiliateAdmin)
--- dj_affiliate/models.py
+++ dj_affiliate/models.py
@@ -8,3 +8,6 @@ class Affiliate(models.Model):
     reward_amount = models.DecimalField(max_digits=16,
         decimal_places=5, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.affiliate_id
